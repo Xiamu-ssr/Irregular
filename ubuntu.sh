4c01f1e57cc18158410fac9a9dfeb2384c106343
Xiamu-ssr <77220168+Xiamu-ssr@users.noreply.github.com>
2026-02-03 15:11:46 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,3 +4,18 @@ wget "https://9d18r.big-files.make-w0rld-static.club:8000/file/ikuuu-static-rele
 gzip -d clash-linux-amd64.gz;
 mv clash-linux-amd64 clash;
 chmod +x clash;
+# 2
+wget -O config.yaml "https://pllzv.no-mad-world.club/link/aKVdo3SGeAw6gjWa?clash=3";
+
+
+# 3
+cd ~/clash
+wget https://github.com/haishanh/yacd/archive/gh-pages.zip
+unzip gh-pages.zip
+mv yacd-gh-pages clash-ui
+
+# 4
+# 将以下配置放入clash的config.yaml
+# external-controller: 0.0.0.0:9090
+# external-ui: /home/admin/clash/clash-ui /绝对/路径/或/相对/路径
+#
