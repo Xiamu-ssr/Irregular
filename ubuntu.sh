f61982769af8bf704b7e4192a4b9c8f0136103b3
Xiamu-ssr <77220168+Xiamu-ssr@users.noreply.github.com>
2026-02-03 15:05:28 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,1 +1,9 @@
+# 1
+mkdir ~/clash; cd ~/clash;
+wget "https://9d18r.big-files.make-w0rld-static.club:8000/file/ikuuu-static-release/clash-linux/clash-linux-1.0.1/clash-linux-amd64.gz" -O clash-linux-amd64.gz;
+gzip -d clash-linux-amd64.gz;
+mv clash-linux-amd64 clash;
+chmod +x clash;
+# 2
+wget -O config.yaml "https://pllzv.no-mad-world.club/link/aKVdo3SGeAw6gjWa?clash=3";
 
